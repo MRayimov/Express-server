c4b2fb698101afdd2800ecf423af230d0cfd1566
MRayimov <mraimov1206@gmail.com>
2024-09-24 10:41:09 +0500

    3

--- App/app.js
+++ App/app.js
@@ -5,16 +5,16 @@ import AppError from "../utils/appError.js";
 import authRoutes from "../routes/authRoutes.js";
 import { errorController } from "../controllers/errorController.js";
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to the server!");
 });
-app.use("/categories", categoriesRoutes);
-app.use("/products", productsRoutes);
-// app.use("/users", authRoutes);
+// app.use("/categories", categoriesRoutes);
+// app.use("/products", productsRoutes);
+app.use("/users", authRoutes);
 app.all("*", (req, res, next) => {
   return next(new AppError(`Can't find ${req.originalUrl} on this server!`));
 });
 
-// app.use(errorController);
+app.use(errorController);
 export default app;
