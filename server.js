eaaff3f19396d63e930dd78b0a9713260ef5dd5d
MRayimov <mraimov1206@gmail.com>
2024-09-24 12:59:22 +0500

    swagger

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 import { configDotenv } from "dotenv";
-import mongoose from "mongoose";
+import mongoose, { version } from "mongoose";
 import serverless from "serverless-http";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 configDotenv({ path: "./config.env" });
 import { app } from "./App/app.js";
-
+const options = {
+  defenition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Welcome",
+      version: "0.1",
+    },
+    servers: [
+      {
+        url: "https://mr-work.netlify.app/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const specs = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 const DB = process.env.DATABASE.replace(
   "<db_password>",
   process.env.DATABASE_PASSWORD
