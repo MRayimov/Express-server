83db747c6fde13fab96f5255ad70eed33c6bc647
MRayimov <mraimov1206@gmail.com>
2024-09-24 14:07:59 +0500

    swag

---
+++ app.js
@@ -0,0 +1,60 @@
+import express from "express";
+import { categoriesRoutes } from "./routes/categoriesRoutes.js";
+import { productsRoutes } from "./routes/productsRoutes.js";
+import AppError from "./utils/appError.js";
+import { authRoutes } from "./routes/authRoutes.js";
+import { errorController } from "./controllers/errorController.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggetUi from "swagger-ui-express";
+import { version } from "mongoose";
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Work Project",
+      version: "1.0.0",
+    },
+    servers: [{ url: "https://mr-work.netlify.app/" }],
+  },
+  apis: [`${import.meta.dirname}/routes/*.js`],
+};
+// Sample route
+/**
+ * @swagger
+ * paths:
+ *  /api/user:
+ *    get:
+ *     summary: Retrieve a list of users
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: John Doe
+ */
+const swaggerSpec = swaggerJSDoc(options);
+export const app = express();
+app.use("/api-docs", express.static("dist/api-docs"));
+app.use("/api-docs", swaggetUi.serve, swaggetUi.setup(swaggerSpec));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!");
+});
+app.use("/categories", categoriesRoutes);
+app.use("/products", productsRoutes);
+app.use("/users", authRoutes);
+app.all("*", (req, res, next) => {
+  return next(new AppError(`Can't find ${req.originalUrl} on this server!`));
+});
+
+app.use(errorController);
