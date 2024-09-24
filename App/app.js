91e26908a6cd1bbc9bdb24dc7bc7a236dea1600e
MRayimov <mraimov1206@gmail.com>
2024-09-24 13:36:17 +0500

    asdf

--- App/app.js
+++ App/app.js
@@ -4,7 +4,24 @@ import { productsRoutes } from "../routes/productsRoutes.js";
 import AppError from "../utils/appError.js";
 import { authRoutes } from "../routes/authRoutes.js";
 import { errorController } from "../controllers/errorController.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggetUi from "swagger-ui-express";
+import { version } from "mongoose";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Work Project",
+      version: "1.0.0",
+    },
+    servers: [{ api: "https://mr-work.netlify.app/" }],
+  },
+  apis: ["../routes/*.js"],
+};
+const swaggerSpec = swaggerJSDoc(options);
 export const app = express();
+app.use("/api-docs", swaggetUi.serve, swaggetUi.setup(swaggerSpec));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to the server!");
--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 import { configDotenv } from "dotenv";
 import mongoose, { version } from "mongoose";
 import serverless from "serverless-http";
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
-import yamljs from "yamljs";
 configDotenv({ path: "./config.env" });
 import { app } from "./App/app.js";
-const swaggerDocument = yamljs.load("./swagger.yaml"); // Adjust path as needed
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const DB = process.env.DATABASE.replace(
   "<db_password>",
