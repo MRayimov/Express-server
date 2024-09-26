77388677e7311432d2c1cff73dcac644654ecc1e
MRayimov <mraimov1206@gmail.com>
2024-09-26 15:13:15 +0500

    renderga qaytamiz

--- utils/upload.js
+++ utils/upload.js
@@ -3,10 +3,10 @@ import path from "path";
 import multer from "multer";
 
 // Create uploads folder if it doesn't exist
-const uploadDir = path.join(__dirname, "../uploads");
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir);
-// }
+const uploadDir = path.join(import.meta.dirname, "../uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 let storage = multer.diskStorage({
   destination: function (req, file, callback) {
